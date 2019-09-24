59ae7ac8b0db1c11059c6aac467bc327ef11cf0f
Rafael Barbosa <rafaelbc@dcomp.ufs.br>
2019-09-24 00:11:12 -0300

    Adicionada area de boxes na home da administracao

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -2,10 +2,59 @@
 
 @section('title', 'AdminLTE')
 
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
-
 @section('content')
-    <p>You are logged in!</p>
+    <div class="row">
+    <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53</h3>
+              <p>Pacientes</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53</h3>
+              <p>Profissionais</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53</h3>
+              <p>Convênios</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53</h3>
+              <p>Exames</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+    </div>
 @stop
--- resources/views/admin/profissionais/index.blade.php
+++ resources/views/admin/profissionais/index.blade.php
@@ -3,9 +3,19 @@
 @section('title', 'Profissionais')
 
 @section('content_header')
-    <h1>Profissionais</h1>
+    <section class="content-header">
+      <h1>Profissionais</h1>
+      <ol class="breadcrumb">
+        <li><a href="{{ url('/') }}"><i class="fa fa-dashboard"></i>Home</a></li>
+        <li class="active">Profissionais</li>
+      </ol>
+    </section>
 @stop
 
 @section('content')
-    <p>Lista de profissionais</p>
+    <div class="box">
+        <div class="box-body">
+            Start creating your amazing application!
+        </div>
+    </div>
 @stop
